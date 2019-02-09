23ddd69cbb057d9494fb524186fa170bea365ddc
Rares <rares.tarabega@global-bit.com>
2019-02-09 04:01:12 +0200

    almost done

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -200,6 +200,183 @@ class DefaultController extends Controller
                 'QA' => 'Qatar',
                 'MZ' => 'Mozambique',
             );
+            $countries2 = array (
+                'BD' => 'Bangladesh',
+                'BE' => 'Belgium',
+                'BF' => 'Burkina%20Faso',
+                'BG' => 'Bulgaria',
+                'BA' => 'Bosnia%20and%20Herzegovina',
+                'BN' => 'Brunei',
+                'BO' => 'Bolivia',
+                'JP' => 'Japan',
+                'BI' => 'Burundi',
+                'BJ' => 'Benin',
+                'BT' => 'Bhutan',
+                'JM' => 'Jamaica',
+                'BW' => 'Botswana',
+                'BR' => 'Brazil',
+                'BS' => 'Bahamas',
+                'BY' => 'Belarus',
+                'BZ' => 'Belize',
+                'RU' => 'Russian%20Federation',
+                'RW' => 'Rwanda',
+                'RS' => 'Serbia',
+                'TL' => 'Timor-Leste',
+                'TM' => 'Turkmenistan',
+                'TJ' => 'Tajikistan',
+                'RO' => 'ROU',
+                'GW' => 'Guinea-Bissau',
+                'GT' => 'Guatemala',
+                'GR' => 'Greece',
+                'GQ' => 'Equatorial%20Guinea',
+                'GY' => 'Guyana',
+                'GE' => 'Georgia',
+                'GB' => 'United%20Kingdom',
+                'GA' => 'Gabon',
+                'GN' => 'Guinea',
+                'GM' => 'Gambia',
+                'GL' => 'Greenland',
+                'GH' => 'Ghana',
+                'OM' => 'Oman',
+                'TN' => 'Tunisia',
+                'JO' => 'Jordan',
+                'HR' => 'Croatia',
+                'HT' => 'Haiti',
+                'HU' => 'Hungary',
+                'HN' => 'Honduras',
+                'PR' => 'Puerto%20Rico',
+                'PS' => 'Palestine',
+                'PT' => 'Portugal',
+                'PY' => 'Paraguay',
+                'PA' => 'Panama',
+                'PG' => 'Papua%20New%20Guinea',
+                'PE' => 'Peru',
+                'PK' => 'Pakistan',
+                'PH' => 'Philippines',
+                'PL' => 'Poland',
+                'ZM' => 'Zambia',
+                'EH' => 'Western%20Sahara',
+                'EE' => 'Estonia',
+                'EG' => 'Egypt',
+                'ZA' => 'South%20Africa',
+                'EC' => 'Ecuador',
+                'IT' => 'Italy',
+                'VN' => 'Vietnam',
+                'SB' => 'Solomon%20Islands',
+                'ET' => 'Ethiopia',
+                'SO' => 'Somalia',
+                'ZW' => 'Zimbabwe',
+                'ES' => 'Spain',
+                'ER' => 'Eritrea',
+                'ME' => 'Montenegro',
+                'MD' => 'Moldova',
+                'MG' => 'Madagascar',
+                'MA' => 'Morocco',
+                'UZ' => 'Uzbekistan',
+                'MM' => 'Myanmar',
+                'ML' => 'Mali',
+                'MN' => 'Mongolia',
+                'MK' => 'Macedonia',
+                'MW' => 'Malawi',
+                'MR' => 'Mauritania',
+                'UG' => 'Uganda',
+                'MY' => 'Malaysia',
+                'MX' => 'Mexico',
+                'IL' => 'Israel',
+                'FR' => 'France',
+                'FI' => 'Finland',
+                'FJ' => 'Fiji',
+                'FK' => 'Falkland%20Islands',
+                'NI' => 'Nicaragua',
+                'NL' => 'Netherlands',
+                'NO' => 'Norway',
+                'NA' => 'Namibia',
+                'VU' => 'Vanuatu',
+                'NC' => 'New%20Caledonia',
+                'NE' => 'Niger',
+                'NG' => 'Nigeria',
+                'NZ' => 'New%20Zealand',
+                'NP' => 'Nepal',
+                'XK' => 'Kosovo',
+                'CI' => 'Cote-d-Ivoire',
+                'CH' => 'Switzerland',
+                'CO' => 'Colombia',
+                'CN' => 'China',
+                'CM' => 'Cameroon',
+                'CL' => 'Chile',
+                'XC' => 'N.Cyprus',
+                'CA' => 'Canada',
+                'CG' => 'Congo',
+                'CF' => 'Central%20African%20Republic',
+                'CD' => 'Dem%20Rep%20of%20Congo',
+                'CZ' => 'Czech%20Republic',
+                'CY' => 'Cyprus',
+                'CR' => 'Costa%20Rica',
+                'CU' => 'Cuba',
+                'SZ' => 'Swaziland',
+                'SY' => 'Syria',
+                'KG' => 'Kyrgyzstan',
+                'KE' => 'Kenya',
+                'SS' => 'South%20Sudan',
+                'SR' => 'Suriname',
+                'KH' => 'Cambodia',
+                'SV' => 'El%20Salvador',
+                'SK' => 'Slovakia',
+                'KR' => 'Rep%20of%20Korea',
+                'SI' => 'Slovenia',
+                'KP' => 'Dem%20Peoples%20Rep%20of%20Korea',
+                'KW' => 'Kuwait',
+                'SN' => 'Senegal',
+                'SL' => 'Sierra%20Leone',
+                'KZ' => 'Kazakhstan',
+                'SA' => 'Saudi%20Arabia',
+                'SE' => 'Sweden',
+                'SD' => 'Sudan',
+                'DO' => 'Dominican%20Republic',
+                'DJ' => 'Djibouti',
+                'DK' => 'Denmark',
+                'DE' => 'Germany',
+                'YE' => 'Yemen',
+                'DZ' => 'Algeria',
+                'US' => 'United%20States',
+                'UY' => 'Uruguay',
+                'LB' => 'Lebanon',
+                'LA' => 'Lao%20PDR',
+                'TW' => 'Taiwan',
+                'TT' => 'Trinidad%20and%20Tobago',
+                'TR' => 'Turkey',
+                'LK' => 'Sri%20Lanka',
+                'LV' => 'Latvia',
+                'LT' => 'Lithuania',
+                'LU' => 'Luxembourg',
+                'LR' => 'Liberia',
+                'LS' => 'Lesotho',
+                'TH' => 'Thailand',
+                'TF' => 'Fr.S.AntarcticLands',
+                'TG' => 'Togo',
+                'TD' => 'Chad',
+                'LY' => 'Libya',
+                'AE' => 'United%20Arab%20Emirates',
+                'VE' => 'RB-de-Venezuela',
+                'AF' => 'Afghanistan',
+                'IQ' => 'Iraq',
+                'IS' => 'Iceland',
+                'IR' => 'Islamic%20Republic%20of%20Iran',
+                'AM' => 'Armenia',
+                'AL' => 'Albania',
+                'AO' => 'Angola',
+                'AR' => 'Argentina',
+                'AU' => 'Australia',
+                'AT' => 'Austria',
+                'IN' => 'India',
+                'TZ' => 'Tanzania',
+                'AZ' => 'Azerbaijan',
+                'IE' => 'Ireland',
+                'ID' => 'Indonesia',
+                'UA' => 'Ukraine',
+                'QA' => 'Qatar',
+                'MZ' => 'Mozambique',
+            );
             $now = new \DateTime();
             $string = file_get_contents("http://api.population.io:80/1.0/population/".$countries[$country]."/".$now->format("Y-m-d"));
             $population=json_decode($string,true);
@@ -216,6 +393,12 @@ class DefaultController extends Controller
             $string = file_get_contents("http://api.population.io:80/1.0/life-expectancy/total/male/".$countries[$country]."/1990-01-01");
             $lifeExpectancyMale = json_decode($string,true);
 
+            $inputTotalPopulation = file_get_contents("https://api.worldbank.org/v2/sources/57/country/".$countries2[$country]."/series/SP.POP.TOTL/time/all/version/201809/data?per_page=6&format=json");
+            $outputTotalPopulation = json_decode($inputTotalPopulation, true);
+
+            $inputGDPcapita = file_get_contents("https://api.worldbank.org/v2/sources/57/country/".$countries2[$country]."/series/NY.GDP.PCAP.CD/time/all/version/201809/data?per_page=12&format=json");
+            $outputGDPcapita = json_decode($inputGDPcapita, true);
+
 
             $beautiful = str_replace("%20"," ",$countries[$country]);
             return $this->render('charts/country.html.twig', array(
@@ -225,6 +408,8 @@ class DefaultController extends Controller
                 'mortalityDistributionMale' => $mortalityDistributionMale,
                 'lifeExpectancyFemale' => $lifeExpectancyFemale,
                 'lifeExpectancyMale' => $lifeExpectancyMale,
+                'outputTotalPopulation' => $outputTotalPopulation,
+                'outputGDPcapita' => $outputGDPcapita,
             ));
         } else {
             return $this->redirect($this->generateUrl('fos_user_security_login'));
@@ -235,9 +420,20 @@ class DefaultController extends Controller
     {
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
 
-            return $this->render('about/about.html.twig');
+            return $this->render('info/about.html.twig');
         } else {
             return $this->redirect($this->generateUrl('fos_user_security_login'));
         }
+    }
+
+    public function privacyAction(Request $request)
+    {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+
+            return $this->render('info/privacy.html.twig');
+        } else {
+            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        }
+
     }
 }
--- web/js/mainApp.js
+++ web/js/mainApp.js
@@ -5,248 +5,248 @@
 
   try {
     //WidgetChart 1
-    var ctx = document.getElementById("widgetChart1");
-    if (ctx) {
-      ctx.height = 130;
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['', '', '', '', '', '', ''],
-          type: 'line',
-          datasets: [{
-            data: [78, 81, 80, 45, 34, 12, 40],
-            label: 'Dataset',
-            backgroundColor: 'rgba(255,255,255,.1)',
-            borderColor: 'rgba(255,255,255,.55)',
-          },]
-        },
-        options: {
-          maintainAspectRatio: true,
-          legend: {
-            display: false
-          },
-          layout: {
-            padding: {
-              left: 0,
-              right: 0,
-              top: 0,
-              bottom: 0
-            }
-          },
-          responsive: true,
-          scales: {
-            xAxes: [{
-              gridLines: {
-                color: 'transparent',
-                zeroLineColor: 'transparent'
-              },
-              ticks: {
-                fontSize: 2,
-                fontColor: 'transparent'
-              }
-            }],
-            yAxes: [{
-              display: false,
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-          title: {
-            display: false,
-          },
-          elements: {
-            line: {
-              borderWidth: 0
-            },
-            point: {
-              radius: 0,
-              hitRadius: 10,
-              hoverRadius: 4
-            }
-          }
-        }
-      });
-    }
+    // var ctx = document.getElementById("widgetChart1");
+    // if (ctx) {
+    //   ctx.height = 130;
+    //   var myChart = new Chart(ctx, {
+    //     type: 'line',
+    //     data: {
+    //       labels: ['', '', '', '', '', '', ''],
+    //       type: 'line',
+    //       datasets: [{
+    //         data: [78, 81, 80, 45, 34, 12, 40],
+    //         label: 'Dataset',
+    //         backgroundColor: 'rgba(255,255,255,.1)',
+    //         borderColor: 'rgba(255,255,255,.55)',
+    //       },]
+    //     },
+    //     options: {
+    //       maintainAspectRatio: true,
+    //       legend: {
+    //         display: false
+    //       },
+    //       layout: {
+    //         padding: {
+    //           left: 0,
+    //           right: 0,
+    //           top: 0,
+    //           bottom: 0
+    //         }
+    //       },
+    //       responsive: true,
+    //       scales: {
+    //         xAxes: [{
+    //           gridLines: {
+    //             color: 'transparent',
+    //             zeroLineColor: 'transparent'
+    //           },
+    //           ticks: {
+    //             fontSize: 2,
+    //             fontColor: 'transparent'
+    //           }
+    //         }],
+    //         yAxes: [{
+    //           display: false,
+    //           ticks: {
+    //             display: false,
+    //           }
+    //         }]
+    //       },
+    //       title: {
+    //         display: false,
+    //       },
+    //       elements: {
+    //         line: {
+    //           borderWidth: 0
+    //         },
+    //         point: {
+    //           radius: 0,
+    //           hitRadius: 10,
+    //           hoverRadius: 4
+    //         }
+    //       }
+    //     }
+    //   });
+    // }
 
 
-    //WidgetChart 2
-    var ctx = document.getElementById("widgetChart2");
-    if (ctx) {
-      ctx.height = 130;
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['', '', '', '', '', ''],
-          type: 'line',
-          datasets: [{
-            data: [1, 18, 9, 17, 34, 22],
-            label: 'Dataset',
-            backgroundColor: 'transparent',
-            borderColor: 'rgba(255,255,255,.55)',
-          },]
-        },
-        options: {
+    // //WidgetChart 2
+    // var ctx = document.getElementById("widgetChart2");
+    // if (ctx) {
+    //   ctx.height = 130;
+    //   var myChart = new Chart(ctx, {
+    //     type: 'line',
+    //     data: {
+    //       labels: ['', '', '', '', '', ''],
+    //       type: 'line',
+    //       datasets: [{
+    //         data: [1, 18, 9, 17, 34, 22],
+    //         label: 'Dataset',
+    //         backgroundColor: 'transparent',
+    //         borderColor: 'rgba(255,255,255,.55)',
+    //       },]
+    //     },
+    //     options: {
 
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          responsive: true,
-          tooltips: {
-            mode: 'index',
-            titleFontSize: 12,
-            titleFontColor: '#000',
-            bodyFontColor: '#000',
-            backgroundColor: '#fff',
-            titleFontFamily: 'Montserrat',
-            bodyFontFamily: 'Montserrat',
-            cornerRadius: 3,
-            intersect: false,
-          },
-          scales: {
-            xAxes: [{
-              gridLines: {
-                color: 'transparent',
-                zeroLineColor: 'transparent'
-              },
-              ticks: {
-                fontSize: 2,
-                fontColor: 'transparent'
-              }
-            }],
-            yAxes: [{
-              display: false,
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-          title: {
-            display: false,
-          },
-          elements: {
-            line: {
-              tension: 0.00001,
-              borderWidth: 1
-            },
-            point: {
-              radius: 4,
-              hitRadius: 10,
-              hoverRadius: 4
-            }
-          }
-        }
-      });
-    }
-
-
-    //WidgetChart 3
-    var ctx = document.getElementById("widgetChart3");
-    if (ctx) {
-      ctx.height = 130;
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['', '', '', '', '', ''],
-          type: 'line',
-          datasets: [{
-            data: [65, 59, 84, 84, 51, 55],
-            label: 'Dataset',
-            backgroundColor: 'transparent',
-            borderColor: 'rgba(255,255,255,.55)',
-          },]
-        },
-        options: {
-
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          responsive: true,
-          tooltips: {
-            mode: 'index',
-            titleFontSize: 12,
-            titleFontColor: '#000',
-            bodyFontColor: '#000',
-            backgroundColor: '#fff',
-            titleFontFamily: 'Montserrat',
-            bodyFontFamily: 'Montserrat',
-            cornerRadius: 3,
-            intersect: false,
-          },
-          scales: {
-            xAxes: [{
-              gridLines: {
-                color: 'transparent',
-                zeroLineColor: 'transparent'
-              },
-              ticks: {
-                fontSize: 2,
-                fontColor: 'transparent'
-              }
-            }],
-            yAxes: [{
-              display: false,
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-          title: {
-            display: false,
-          },
-          elements: {
-            line: {
-              borderWidth: 1
-            },
-            point: {
-              radius: 4,
-              hitRadius: 10,
-              hoverRadius: 4
-            }
-          }
-        }
-      });
-    }
+    //       maintainAspectRatio: false,
+    //       legend: {
+    //         display: false
+    //       },
+    //       responsive: true,
+    //       tooltips: {
+    //         mode: 'index',
+    //         titleFontSize: 12,
+    //         titleFontColor: '#000',
+    //         bodyFontColor: '#000',
+    //         backgroundColor: '#fff',
+    //         titleFontFamily: 'Montserrat',
+    //         bodyFontFamily: 'Montserrat',
+    //         cornerRadius: 3,
+    //         intersect: false,
+    //       },
+    //       scales: {
+    //         xAxes: [{
+    //           gridLines: {
+    //             color: 'transparent',
+    //             zeroLineColor: 'transparent'
+    //           },
+    //           ticks: {
+    //             fontSize: 2,
+    //             fontColor: 'transparent'
+    //           }
+    //         }],
+    //         yAxes: [{
+    //           display: false,
+    //           ticks: {
+    //             display: false,
+    //           }
+    //         }]
+    //       },
+    //       title: {
+    //         display: false,
+    //       },
+    //       elements: {
+    //         line: {
+    //           tension: 0.00001,
+    //           borderWidth: 1
+    //         },
+    //         point: {
+    //           radius: 4,
+    //           hitRadius: 10,
+    //           hoverRadius: 4
+    //         }
+    //       }
+    //     }
+    //   });
+    // }
 
 
-    //WidgetChart 4
-    var ctx = document.getElementById("widgetChart4");
-    if (ctx) {
-      ctx.height = 115;
-      var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['', '', '', '', '', '', '', '', '', '', '', ''],
-          datasets: [
-            {
-              label: "Dataset",
-              data: [78, 81, 80, 65, 58, 75, 60, 75, 65, 60, 60, 75],
-              borderColor: "transparent",
-              borderWidth: "0",
-              backgroundColor: "rgba(255,255,255,.3)"
-            }
-          ]
-        },
-        options: {
-          maintainAspectRatio: true,
-          legend: {
-            display: false
-          },
-          scales: {
-            xAxes: [{
-              display: false,
-              categoryPercentage: 1,
-              barPercentage: 0.65
-            }],
-            yAxes: [{
-              display: false
-            }]
-          }
-        }
-      });
-    }
+    // //WidgetChart 3
+    // var ctx = document.getElementById("widgetChart3");
+    // if (ctx) {
+    //   ctx.height = 130;
+    //   var myChart = new Chart(ctx, {
+    //     type: 'line',
+    //     data: {
+    //       labels: ['', '', '', '', '', ''],
+    //       type: 'line',
+    //       datasets: [{
+    //         data: [65, 59, 84, 84, 51, 55],
+    //         label: 'Dataset',
+    //         backgroundColor: 'transparent',
+    //         borderColor: 'rgba(255,255,255,.55)',
+    //       },]
+    //     },
+    //     options: {
+
+    //       maintainAspectRatio: false,
+    //       legend: {
+    //         display: false
+    //       },
+    //       responsive: true,
+    //       tooltips: {
+    //         mode: 'index',
+    //         titleFontSize: 12,
+    //         titleFontColor: '#000',
+    //         bodyFontColor: '#000',
+    //         backgroundColor: '#fff',
+    //         titleFontFamily: 'Montserrat',
+    //         bodyFontFamily: 'Montserrat',
+    //         cornerRadius: 3,
+    //         intersect: false,
+    //       },
+    //       scales: {
+    //         xAxes: [{
+    //           gridLines: {
+    //             color: 'transparent',
+    //             zeroLineColor: 'transparent'
+    //           },
+    //           ticks: {
+    //             fontSize: 2,
+    //             fontColor: 'transparent'
+    //           }
+    //         }],
+    //         yAxes: [{
+    //           display: false,
+    //           ticks: {
+    //             display: false,
+    //           }
+    //         }]
+    //       },
+    //       title: {
+    //         display: false,
+    //       },
+    //       elements: {
+    //         line: {
+    //           borderWidth: 1
+    //         },
+    //         point: {
+    //           radius: 4,
+    //           hitRadius: 10,
+    //           hoverRadius: 4
+    //         }
+    //       }
+    //     }
+    //   });
+    // }
+
+
+    // //WidgetChart 4
+    // var ctx = document.getElementById("widgetChart4");
+    // if (ctx) {
+    //   ctx.height = 115;
+    //   var myChart = new Chart(ctx, {
+    //     type: 'bar',
+    //     data: {
+    //       labels: ['', '', '', '', '', '', '', '', '', '', '', ''],
+    //       datasets: [
+    //         {
+    //           label: "Dataset",
+    //           data: [78, 81, 80, 65, 58, 75, 60, 75, 65, 60, 60, 75],
+    //           borderColor: "transparent",
+    //           borderWidth: "0",
+    //           backgroundColor: "rgba(255,255,255,.3)"
+    //         }
+    //       ]
+    //     },
+    //     options: {
+    //       maintainAspectRatio: true,
+    //       legend: {
+    //         display: false
+    //       },
+    //       scales: {
+    //         xAxes: [{
+    //           display: false,
+    //           categoryPercentage: 1,
+    //           barPercentage: 0.65
+    //         }],
+    //         yAxes: [{
+    //           display: false
+    //         }]
+    //       }
+    //     }
+    //   });
+    // }
 
     // Recent Report
     const brandProduct = 'rgba(0,181,233,0.8)'
